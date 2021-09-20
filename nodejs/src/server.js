316559afa16dc5c6befbc604ad6e92b9b28b1075
alexbaron <alexbaron77@gmail.com>
2021-09-20 17:10:57 -0400

    tuto de base (routing simple)

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -1,59 +1,49 @@
 "use strict";
 
 const express = require("express");
-var React = require("react");
-// var router = express.Router();
+const morgan = require("morgan");
+// var React = require("react");
+var router = express.Router();
 
 // Constants
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
-// App
-const app = express();
+// server
+const server = express();
 //templating
-app.set("views", __dirname + "/views");
-app.set("view engine", "jsx");
-app.engine("jsx", require("express-react-views").createEngine());
+// server.set("views", __dirname + "/views");
+// server.set("view engine", "jsx");
+// server.engine("jsx", require("express-react-views").createEngine());
+
+server.use(morgan("dev"));
 
 //routing
-app.get("/", (req, res) => {
-  res.send("Hello World 0");
+server.get("/", (req, res) => {
+  res.send("Hello World 2 " + req.path);
+  console.log(req.query);
+  console.log("call from client");
 });
 
-app.get("/about", (req, res) => {
-  res.send("About this tuto function flechee");
+server.post("/post", (req, res) => {
+  res.send("post req");
+});
+server.get("/get", (req, res) => {
+  res.send("get req");
 });
 
-// app.get('/about', function (req, res) {
-//   res.send('about');
-// });
+server.use((req, res, suite) => {
+  const error = new Error("404 page not found");
+  error.status = 404;
+  suite(error);
+});
 
-class Screen {
-  width;
-  height;
+server.use((error, req, res) => {
+  res.status(error.status || 500);
+  res.end(error.message);
+});
 
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-  }
-
-  diagonal() {
-    return Math.sqrt(Math.pow(this.width, 2) + Math.pow(this.height, 2));
-  }
-
-  dimensions(definition) {
-    var dimensions = definition.split("x");
-    this.width = parseInt(dimensions[0]);
-    this.height = parseInt(dimensions[1]);
-  }
-}
-
-var screen = new Screen(0, 0);
-screen.dimensions = "500x300";
-screen.width = 400;
-console.log(screen.diagonal);
-
-app.listen(PORT, HOST);
+server.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
-// module.exports = router;
+module.exports = router;
