73740b44e0f4d8311966de86060f3cf5a5affb69
alexbaron <alexbaron77@gmail.com>
2021-09-20 15:46:42 -0400

    config

--- react/api/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET api. */
-router.get('/', function(req, res, next) {
-  res.json({ message: 'Hello from the API server!' });
-});
-
-module.exports = router;
